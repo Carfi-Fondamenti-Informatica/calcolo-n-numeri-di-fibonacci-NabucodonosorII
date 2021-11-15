01083d5f61b87b8f9973b31b1fe93ec94f73421d
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-11-15 04:49:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;int j=1;int k=1;int l;
+    cin >> n;
+    if(n>=2){cout<< j << endl << k << endl;
+        for(int i=2;i<n;i++){l=j+k;j=k;k=l;cout<<l<<endl;
+        }
+    }else{
+        cout<<"errore"<<endl;}
+    return 0;
 }
